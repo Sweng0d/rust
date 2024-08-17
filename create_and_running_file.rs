de820069f5ebc5c329ea26cc7a4c33416264ba9d
Bruno Filetti <101097089+Sweng0d@users.noreply.github.com>
2024-08-17 14:26:59 -0300

    Create create_and_running_file.rs

---
+++ create_and_running_file.rs
@@ -0,0 +1,19 @@
+Create project inside the file:
+
+$ mkdir ~/projects
+$ cd ~/projects
+$ mkdir hello_world
+$ cd hello_world
+
+
+Create a filename called: main.rs
+
+Code:
+fn main() {
+    println!("Hello, world!");
+}
+
+Run the code:
+$ rustc main.rs
+$ ./main
+Hello, world!
