f71bfb8d2ce277a2acb80eab0e078d186adf1957
Bruno Filetti <101097089+Sweng0d@users.noreply.github.com>
2024-08-18 16:10:15 -0300

    Create variables.rs

---
+++ variables.rs
@@ -0,0 +1,19 @@
+
+
+This conde will have an error because X is not mutable and you are changing it
+fn main() {
+    let x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}
+
+
+How to fix:
+
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}
