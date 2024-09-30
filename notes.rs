e7808bf3c01c834ffe8868e1400f8d6991ae39f6
Bruno Filetti <101097089+Sweng0d@users.noreply.github.com>
2024-09-30 17:52:34 -0300

    Create notes.rs

---
+++ notes.rs
@@ -0,0 +1,3 @@
+Char → 
+
+Em Rust, `char` é um tipo de dado que representa um único caractere Unicode, podendo incluir letras, números, símbolos, ou até mesmo emojis. Cada `char` em Rust ocupa 4 bytes e suporta caracteres de várias línguas e sistemas de escrita, diferentemente de tipos mais limitados como `char` em outras linguagens, que geralmente representam apenas caracteres ASCII. Por exemplo, você pode declarar um caractere assim
